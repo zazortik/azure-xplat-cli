7a9f52790880ad8996fec19bcd63cb0614f4d1ac
Tomasz Janczuk <tomasz@janczuk.org>
2012-11-14 23:22:10 -0800

    add azure mobile tests for configuration and table

--- test/commands/cli.mobile-tests.js
+++ test/commands/cli.mobile-tests.js
@@ -19,6 +19,7 @@ var uuid = require('node-uuid');
 var util = require('util');
 var cli = require('../cli');
 var capture = require('../util').capture;
+var https = require('https');
 
 suite('azure mobile', function(){
 
@@ -34,7 +35,7 @@ suite('azure mobile', function(){
     });
   });
 
-  test('create ' + servicename + ' tjanczuk FooBar#12 --json', function(done) {
+  test('create ' + servicename + ' tjanczuk FooBar#12 --json (create new service)', function(done) {
     var cmd = ('node cli.js mobile create ' + servicename + ' tjanczuk FooBar#12 --json').split(' ');
     capture(function() {
       cli.parse(cmd);
@@ -48,7 +49,7 @@ suite('azure mobile', function(){
     });
   });
 
-  test('list --json (contains service)', function(done) {
+  test('list --json (contains healthy service)', function(done) {
     var cmd = ('node cli.js mobile list --json').split(' ');
     capture(function() {
       cli.parse(cmd);
@@ -62,7 +63,7 @@ suite('azure mobile', function(){
     });
   });  
 
-  test('show ' + servicename + ' --json', function(done) {
+  test('show ' + servicename + ' --json (contains healthy service)', function(done) {
     var cmd = ('node cli.js mobile show ' + servicename + ' --json').split(' ');
     capture(function() {
       cli.parse(cmd);
@@ -78,7 +79,7 @@ suite('azure mobile', function(){
     });
   });  
 
-  test('config list ' + servicename + ' --json', function(done) {
+  test('config list ' + servicename + ' --json (default config)', function(done) {
     var cmd = ('node cli.js mobile config list ' + servicename + ' --json').split(' ');
     capture(function() {
       cli.parse(cmd);
@@ -110,7 +111,7 @@ suite('azure mobile', function(){
     });
   });     
 
-  test('config get ' + servicename + ' facebookClientId --json', function(done) {
+  test('config get ' + servicename + ' facebookClientId --json (value was set)', function(done) {
     var cmd = ('node cli.js mobile config get ' + servicename + ' facebookClientId --json').split(' ');
     capture(function() {
       cli.parse(cmd);
@@ -122,7 +123,7 @@ suite('azure mobile', function(){
     });
   });     
 
-  test('table list ' + servicename + ' --json', function(done) {
+  test('table list ' + servicename + ' --json (no tables by default)', function(done) {
     var cmd = ('node cli.js mobile table list ' + servicename + ' --json').split(' ');
     capture(function() {
       cli.parse(cmd);
@@ -135,7 +136,232 @@ suite('azure mobile', function(){
     });
   });      
 
-  test('delete ' + servicename + ' -a -q --json', function(done) {
+  test('table create ' + servicename + ' table1 --json (add first table)', function(done) {
+    var cmd = ('node cli.js mobile table create ' + servicename + ' table1 --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(0);
+      result.text.should.equal('');
+      done();
+    });
+  });    
+
+  test('table list ' + servicename + ' --json (contains one table)', function(done) {
+    var cmd = ('node cli.js mobile table list ' + servicename + ' --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(0);
+      var response = JSON.parse(result.text);
+      Array.isArray(response).should.be.ok;
+      response.length.should.equal(1);
+      response[0].name.should.equal('table1')
+      done();
+    });
+  });      
+
+  test('table show ' + servicename + ' table1 --json (default table config)', function(done) {
+    var cmd = ('node cli.js mobile table show ' + servicename + ' table1 --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(0);
+      var response = JSON.parse(result.text);
+      ['insert', 'read', 'update', 'delete'].forEach(function (permission) {
+        response.permissions[permission].should.equal('application');
+      });
+      response.table.name.should.equal('table1');
+      Array.isArray(response.columns).should.be.ok;
+      response.columns.length.should.equal(1);
+      response.columns[0].name.should.equal('id');
+      done();
+    });
+  });
+
+  test('table update ' + servicename + ' table1 -p *=admin,insert=public --json (update permissions)', function(done) {
+    var cmd = ('node cli.js mobile table update ' + servicename + ' table1 -p *=admin,insert=public --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(0);
+      result.text.should.equal('');
+      done();
+    });
+  });   
+
+  test('table show ' + servicename + ' table1 --json (updated permissions)', function(done) {
+    var cmd = ('node cli.js mobile table show ' + servicename + ' table1 --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(0);
+      var response = JSON.parse(result.text);
+      ['read', 'update', 'delete'].forEach(function (permission) {
+        response.permissions[permission].should.equal('admin');
+      });
+      response.permissions.insert.should.equal('public');
+      response.table.name.should.equal('table1');
+      Array.isArray(response.columns).should.be.ok;
+      response.columns.length.should.equal(1);
+      response.columns[0].name.should.equal('id');
+      done();
+    });
+  });  
+
+  test('(add 5 rows of data to table with public insert permission)', function(done) {
+    var success = 0;
+    var failure = 0;
+
+    function tryFinish() {
+      if ((success + failure) < 5) {
+        return;
+      }
+
+      failure.should.equal(0);
+      done();
+    }
+
+    for (var i = 0; i < 5; i++) {
+      var options = {
+        host: servicename + '.azure-mobile.net',
+        port: 443,
+        path: '/tables/table1',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+
+      var req = https.request(options, function (res) {
+        res.statusCode >= 400 ? failure++ : success++;
+        tryFinish();
+      });
+
+      req.on('error', function () {
+        failure++;
+        tryFinish();
+      });
+
+      req.end(JSON.stringify({ rowNumber: i, foo: 'foo', bar: 7, baz: true }));
+    }
+  });    
+
+  test('table show ' + servicename + ' table1 --json (new rows and columns)', function(done) {
+    var cmd = ('node cli.js mobile table show ' + servicename + ' table1 --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(0);
+      var response = JSON.parse(result.text);
+      response.table.metrics.recordCount.should.equal(5);
+      Array.isArray(response.columns).should.be.ok;
+      response.columns.length.should.equal(5);
+      [ { name: 'id', indexed: true },
+        { name: 'rowNumber', indexed: false },
+        { name: 'foo', indexed: false },
+        { name: 'bar', indexed: false },
+        { name: 'baz', indexed: false } ].forEach(function (column, columnIndex) {
+          response.columns[columnIndex].name.should.equal(column.name);
+          response.columns[columnIndex].indexed.should.equal(column.indexed);
+        });
+      done();
+    });
+  });    
+
+  test('table data ' + servicename + ' table1 --json (show 5 rows of data)', function(done) {
+    var cmd = ('node cli.js mobile table data ' + servicename + ' table1 --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(0);
+      var response = JSON.parse(result.text);
+      Array.isArray(response).should.be.ok;
+      response.length.should.equal(5);
+      done();
+    });
+  });    
+
+  test('table data ' + servicename + ' table1 --top 1 --json (show top 1 row of data)', function(done) {
+    var cmd = ('node cli.js mobile table data ' + servicename + ' table1 --top 1 --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(0);
+      var response = JSON.parse(result.text);
+      Array.isArray(response).should.be.ok;
+      response.length.should.equal(1);
+      done();
+    });
+  });    
+
+  test('table update ' + servicename + ' table1 --deleteColumn foo --addIndex bar,baz -q --json (delete column, add indexes)', function(done) {
+    var cmd = ('node cli.js mobile table update ' + servicename + ' table1  --deleteColumn foo --addIndex bar,baz -q --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(0);
+      result.text.should.equal('');
+      done();
+    });
+  });   
+
+  test('table show ' + servicename + ' table1 --json (fewer columns, more indexes)', function(done) {
+    var cmd = ('node cli.js mobile table show ' + servicename + ' table1 --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(0);
+      var response = JSON.parse(result.text);
+      Array.isArray(response.columns).should.be.ok;
+      response.columns.length.should.equal(4);
+      [ { name: 'id', indexed: true },
+        { name: 'rowNumber', indexed: false },
+        { name: 'bar', indexed: true },
+        { name: 'baz', indexed: true } ].forEach(function (column, columnIndex) {
+          response.columns[columnIndex].name.should.equal(column.name);
+          response.columns[columnIndex].indexed.should.equal(column.indexed);
+        });
+      done();
+    });
+  });    
+
+  test('table delete ' + servicename + ' table1 -q --json (delete existing table)', function(done) {
+    var cmd = ('node cli.js mobile table delete ' + servicename + ' table1 -q --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.text.should.equal('');
+      result.exitStatus.should.equal(0);
+      done();
+    });
+  });    
+
+  test('table list ' + servicename + ' --json (no tables after table deletion)', function(done) {
+    var cmd = ('node cli.js mobile table list ' + servicename + ' --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(0);
+      var response = JSON.parse(result.text);
+      Array.isArray(response).should.be.ok;
+      response.length.should.equal(0);
+      done();
+    });
+  });      
+
+  test('table delete ' + servicename + ' table1 -q --json (delete nonexisting table)', function(done) {
+    var cmd = ('node cli.js mobile table delete ' + servicename + ' table1 -q --json').split(' ');
+    capture(function() {
+      cli.parse(cmd);
+    }, function (result) {
+      result.exitStatus.should.equal(1);
+      result.stderrText.should.include('The table \'table1\' was not found');
+      done();
+    });
+  });    
+
+  test('delete ' + servicename + ' -a -q --json (delete existing service)', function(done) {
     var cmd = ('node cli.js mobile delete ' + servicename + ' -a -q --json').split(' ');
     capture(function() {
       cli.parse(cmd);
@@ -146,7 +372,7 @@ suite('azure mobile', function(){
     });
   });  
 
-  test('list --json (does not contain service)', function(done) {
+  test('list --json (no services exist)', function(done) {
     var cmd = ('node cli.js mobile list --json').split(' ');
     capture(function() {
       cli.parse(cmd);
@@ -160,7 +386,7 @@ suite('azure mobile', function(){
     });
   });      
 
-  test('delete ' + servicename + ' -a -q --json (nonexistent service)', function(done) {
+  test('delete ' + servicename + ' -a -q --json (delete nonexisting service)', function(done) {
     var cmd = ('node cli.js mobile delete ' + servicename + ' -a -q --json').split(' ');
     capture(function() {
       cli.parse(cmd);
