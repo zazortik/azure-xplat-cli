180ae668e3492abcdf35440ea6177b4c6ba13680
Amit Apple <amitap@microsoft.com>
2012-11-21 13:59:37 -0800

    Added error stream capturing to the util.capture function.

--- test/util.js
+++ test/util.js
@@ -20,19 +20,26 @@ exports = module.exports = {
 function capture(action, cb) {
   var result = {
     text: '',
+    errorText: '',
   }
 
   var processStdoutWrite = process.stdout.write
+  var processStderrWrite = process.stderr.write
   var processExit = process.exit
 
   process.stdout.write = function(data, encoding, fd) {
     result.text += data;
   };
 
+  process.stderr.write = function (data, encoding, fd) {
+      result.errorText += data;
+  };
+
   process.exit = function(status) {
     result.exitStatus = status;
 
     process.stdout.write = processStdoutWrite;
+    process.stderr.write = processStderrWrite;
     process.exit = processExit;
 
     return cb(result);
@@ -44,6 +51,7 @@ function capture(action, cb) {
     result.error = err;
 
     process.stdout.write = processStdoutWrite;
+    process.stderr.write = processStderrWrite;
     process.exit = processExit;
 
     cb(result);
