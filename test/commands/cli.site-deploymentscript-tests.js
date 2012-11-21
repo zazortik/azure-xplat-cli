b493f69b7e75673aad9f7df44672478f0f5baa98
Amit Apple <amitap@microsoft.com>
2012-11-20 17:54:41 -0800

    First test for site deploymentscript.

--- test/commands/cli.site-deploymentscript-tests.js
+++ test/commands/cli.site-deploymentscript-tests.js
@@ -18,30 +18,120 @@ var should = require('should');
 var cli = require('../cli');
 var capture = require('../util').capture;
 
-suite('cli', function(){
+var fs = require('fs');
+var pathUtil = require('path');
+if (!fs.existsSync) {
+    fs.existsSync = pathUtil.existsSync;
+};
+
+var baseTestTempDir = "temp";
+var testDirBase = "test";
+var testDirIndex = 0;
+var testDir = "";
+
+suite('cli', function() {
   suite('site deploymentscript', function() {
-      teardown(function (done) {
-          done();
-    });
-
-    test('generate node deployment script', function(done) {
-      // Create site
-      var cmd = ('node cli.js site deploymentscript --php').split(' ');
-
-      capture(function() {
-          console.log('b');
-          cli.parse(cmd);
-      }, function (result) {
-          try {
-              //result.exitStatus.should.equal(0);
-              // result.text.should.equal('');
-              done();
-          }
-          catch (e) {
-              console.log(e);
-              done(e);
-          }
+      setup(function () {
+          testDirIndex++;
+          testDir = pathUtil.join(baseTestTempDir, testDirBase + testDirIndex);
+          ensurePathExists(testDir);
       });
+
+      teardown(function () {
+          removePath(testDir);
+      });
+
+      test('generate php deployment script (--php -y -r)', function (done) {
+        // Create site
+        var cmd = ('node cli.js site deploymentscript --php -y -r ' + testDir).split(' ');
+
+        capture(function () {
+            cli.parse(cmd);
+        }, function (result) {
+            try {
+                console.log(result.text);
+
+                result.exitStatus.should.equal(0);
+
+                result.text.should.include('Generating deployment script for Web Site');
+                result.text.should.include('Generated deployment script (deploy.cmd and .deployment)');
+
+                var deployCmdContent = getFileContent('deploy.cmd');
+                deployCmdContent.should.include('echo Handling Basic Web Site deployment.');
+
+                var deploymentFileContent = getFileContent('.deployment');
+                deploymentFileContent.should.include('deploy.cmd');
+
+                done();
+            }
+            catch (e) {
+                console.log(e);
+                done(e);
+            }
+        });
     });
   });
 });
+
+function getFileContent(path) {
+    path = pathUtil.join(testDir, path);
+    fs.existsSync(path).should.equal(true, "File doesn't exist: " + path);
+    return fs.readFileSync(path, 'utf8');
+}
+
+function removePath(path) {
+    var stat = tryGetFileStat(path);
+    if (stat) {
+        if (!stat.isDirectory()) {
+            tryRemoveFile(path);
+        }
+        else {
+            var files = fs.readdirSync(path);
+            for (var index in files) {
+                var file = files[index];
+                var filePath = pathUtil.join(path, file);
+                removePath(filePath);
+            }
+
+            tryRemoveDir(path);
+        }
+    }
+}
+
+function tryGetFileStat(path) {
+    try {
+        return fs.statSync(path);
+    }
+    catch (e) {
+        if (e.errno == 34) {
+            // Return null if path doesn't exist
+            return null;
+        }
+
+        throw e;
+    }
+}
+
+function tryRemoveFile(path) {
+    try {
+        fs.unlinkSync(path);
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+function tryRemoveDir(path) {
+    try {
+        fs.rmdirSync(path);
+    }
+    catch (e) {
+    }
+}
+
+function ensurePathExists(path) {
+    if (!fs.existsSync(path)) {
+        ensurePathExists(pathUtil.dirname(path));
+        fs.mkdirSync(path);
+    }
+}
