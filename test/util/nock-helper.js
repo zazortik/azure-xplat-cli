76ec73cfc9f323a814162512c4cadf208477b280
Andre Rodrigues <andrerod@microsoft.com>
2013-01-24 21:26:10 -0800

    Fix styling issues

--- test/util/nock-helper.js
+++ test/util/nock-helper.js
@@ -13,15 +13,15 @@
 * limitations under the License.
 */
 
-var https = require('https'),
-    http = require('http'),
-    OriginalClientRequest = http.ClientRequest, // HTTP ClientRequest before mocking by Nock
-    OriginalHttpsRequest = https.request,
-    OriginalHttpRequest = http.request,
-    nock = require('nock'),
-    NockClientRequest = http.ClientRequest, // HTTP ClientRequest mocked by Nock
-    NockHttpsRequest = https.request,
-    NockHttpRequest = http.request;
+var https = require('https');
+var http = require('http');
+var OriginalClientRequest = http.ClientRequest; // HTTP ClientRequest before mocking by Nock
+var OriginalHttpsRequest = https.request;
+var OriginalHttpRequest = http.request;
+var nock = require('nock');
+var NockClientRequest = http.ClientRequest; // HTTP ClientRequest mocked by Nock
+var NockHttpsRequest = https.request;
+var NockHttpRequest = http.request;
 
 // The nock module should only be required once in all of the test infrastructure.
 // If the nock require/OriginalClientRequest/NockClientRequest dance is done in multiple
@@ -34,15 +34,15 @@ var https = require('https'),
 exports.nock = nock;
 
 exports.nockHttp = function() {
-    http.ClientRequest = NockClientRequest;
-    http.request = NockHttpRequest;
-    https.request = NockHttpsRequest;
+  http.ClientRequest = NockClientRequest;
+  http.request = NockHttpRequest;
+  https.request = NockHttpsRequest;
 };
 
 exports.unNockHttp = function() {
-    http.ClientRequest = OriginalClientRequest;
-    http.request = OriginalHttpRequest;
-    https.request = OriginalHttpsRequest;
+  http.ClientRequest = OriginalClientRequest;
+  http.request = OriginalHttpRequest;
+  https.request = OriginalHttpsRequest;
 };
 
 exports.unNockHttp(); // Revert the nock change so that tests by default run with the original, unmocked http request objects
