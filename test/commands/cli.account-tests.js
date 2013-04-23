86aa34a28f0546ce2d7fb89599a17eb59d1f51c8
Andre Rodrigues <andrerod@microsoft.com>
2013-04-23 11:12:14 +0100

    Fix travis

--- test/commands/cli.account-tests.js
+++ test/commands/cli.account-tests.js
@@ -17,26 +17,116 @@ require('should');
 var sinon = require('sinon');
 var fs = require('fs');
 
+var keyFiles = require('../../lib/keyFiles');
+var utils = require('../../lib/utils');
 var executeCmd = require('../framework/cli-executor').execute;
+
+var testFile = './test/data/account-credentials.publishsettings';
 
 describe('cli', function(){
   describe('account', function() {
     describe('import', function() {
       beforeEach(function (done) {
-        sinon.stub(fs, 'writeFileSync', function () {});
+        var currentCertificate = process.env.AZURE_CERTIFICATE;
+        var currentCertificateKey = process.env.AZURE_CERTIFICATE_KEY;
+
+        var realRead = keyFiles.readFromFile;
+
+        var filesExist = [];
+
+        sinon.stub(fs, 'mkdirSync', function () { });
+
+        sinon.stub(fs, 'writeFileSync', function (filename, data) {
+          filesExist.push({ name: filename, data: data });
+        });
+
+        sinon.stub(fs, 'writeFile', function (filename, data, callback) { callback(); });
+
+        var originalReadFileSync = fs.readFileSync;
+        sinon.stub(fs, 'readFileSync', function (filename) {
+          if (filename === testFile) {
+            return originalReadFileSync(filename, 'utf8');
+          } else {
+            return filesExist.filter(function (f) {
+              return f.name === filename;
+            })[0].data;
+          }
+        });
+
+        sinon.stub(utils, 'writeFileSyncMode', function (filename, data) {
+          filesExist.push({ name: filename, data: data });
+        });
+
+        sinon.stub(utils, 'pathExistsSync', function (filename) {
+          return filesExist.some(function (f) {
+            return f.name === filename;
+          });
+        });
+
+        sinon.stub(keyFiles, 'readFromFile', function (filename) {
+          if (filename === testFile) {
+            var data = realRead(filename);
+            return data;
+          } else {
+            return {
+              cert: currentCertificate,
+              key: currentCertificateKey
+            };
+          }
+        });
+
+        sinon.stub(keyFiles, 'writeToFile', function (filename, data) {
+          currentCertificateKey = data.key;
+          currentCertificate = data.cert;
+        });
+
         done();
       });
 
       afterEach(function (done) {
-        fs.writeFileSync.restore();
+        if (keyFiles.readFromFile.restore) {
+          keyFiles.readFromFile.restore();
+        }
+
+        if (keyFiles.writeToFile.restore) {
+          keyFiles.writeToFile.restore();
+        }
+
+        if (utils.writeFileSyncMode.restore) {
+          utils.writeFileSyncMode.restore();
+        }
+
+        if (utils.pathExistsSync.restore) {
+          utils.pathExistsSync.restore();
+        }
+
+        if (fs.mkdirSync.restore) {
+          fs.mkdirSync.restore();
+        }
+
+        if (fs.writeFileSync.restore) {
+          fs.writeFileSync.restore();
+        }
+
+        if (fs.writeFile.restore) {
+          fs.writeFile.restore();
+        }
+
+        if (fs.readFile.restore) {
+          fs.readFile.restore();
+        }
+
+        if (fs.readFileSync.restore) {
+          fs.readFileSync.restore();
+        }
+
         done();
       });
 
-      it('should not import invalid certificate', function(done) {
-        var cmd = 'node cli.js account import ./test/data/account-credentials.publishsettings'.split(' ');
+      it('should import certificate', function(done) {
+        var cmd = ('node cli.js account import ' + testFile + ' --skipregister').split(' ');
         executeCmd(cmd, function (result) {
-          result.errorText.should.match(/The server failed to authenticate the request. Verify that the certificate is valid and is associated with this subscription/);
-          result.exitStatus.should.equal(1);
+          result.exitStatus.should.equal(0);
           done();
         });
       });
