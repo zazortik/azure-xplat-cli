954a9b1cc7e5ec62236d32fe3816c4007937b0b8
Chris Tavares <ctavares@microsoft.com>
2013-05-28 13:51:05 -0700

    Initial step scaffolding

---
+++ features/step_definitions/website-credential-steps.js
@@ -0,0 +1,50 @@
+/**
+* Copyright (c) Microsoft.  All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+'use strict';
+
+function websiteCredentialSteps() {
+  this.Given(/^a one subscription publishsettings file$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I import the publishsettings file$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^current subscription is set correctly$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^management endpoint is set correctly$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I list Websites$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^the command succeeds$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+}
+
+module.exports = websiteCredentialSteps;
