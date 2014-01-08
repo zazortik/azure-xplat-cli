8aa3959231fbc492877e482cdee344e7dfc38c51
Rajeeva <acharya.rajiv@gmail.com>
2014-01-08 12:07:20 +0530

    Testsuite for vm commands by MSOpenTech
    Testsuite for vm commands by MSOpenTech
    TestSuite for azure vm commands
    Current code coverage for vm._js is 70%
    Skipping subscription for vm location

--- test/framework/cli-test.js
+++ test/framework/cli-test.js
@@ -1,18 +1,17 @@
-// 
-// Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//   http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// 
+/**
+* Copyright (c) Microsoft.  All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 // Test includes
 var fs = require('fs');
@@ -160,7 +159,7 @@ _.extend(CLITest.prototype, {
       cmd.unshift('node');
     }
 
-    if (!this.skipSubscription && this.isMocked && !this.isRecording) {
+    if (!this.skipSubscription && this.isMocked && !this.isRecording && cmd[2] != 'vm' && cmd[3] != 'location') {
       cmd.push('-s');
       cmd.push(process.env.AZURE_SUBSCRIPTION_ID);
     }
