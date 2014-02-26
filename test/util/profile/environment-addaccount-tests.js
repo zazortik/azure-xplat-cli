75c47e963f909460d321a4c1eccc9a44174a822e
Chris Tavares <ctavares@microsoft.com>
2014-02-25 21:54:42 -0800

    Updated profile tests to include listsubscriptions.

--- test/util/profile/environment-addaccount-tests.js
+++ test/util/profile/environment-addaccount-tests.js
@@ -18,54 +18,88 @@
 var _ = require('underscore');
 var should = require('should');
 var sinon = require('sinon');
+var util = require('util');
 
 var constants = require('../../../lib/util/constants');
 var profile = require('../../../lib/util/profile');
 
+var expectedSubscriptions = [
+  {
+    subscriptionId: 'id0',
+    subscriptionName: 'name0'
+  },
+  {
+    subscriptionId: 'id1',
+    subscriptionName: 'name1'
+  }
+];
+
+var expectedToken = {
+  accessToken: 'a dummy token',
+  expiresOn: new Date(Date.now() + 5 * 60 * 1000)
+};
+
 describe('Environment', function () {
   var environment;
 
-  function tokenProvider(authConfig, username, password, callback) {
-    callback(null, {
-      accessToken: 'a dummy token',
-      expiresOn: new Date(Date.now() + 5 * 60 * 1000)
-    });
-  }
-
   before(function () {
     environment = new profile.Environment({ name: 'TestEnvironment' });
-    environment.acquireToken = sinon.spy(tokenProvider);
+    sinon.stub(environment, 'acquireToken').callsArgWith(3, null, expectedToken);
+    sinon.stub(environment, 'getAccountSubscriptions').callsArgWith(1, null, expectedSubscriptions);
   });
 
   describe('When creating account', function () {
-    var subscription;
+    var subscriptions;
 
     beforeEach(function (done) {
-      environment.addAccount('user@somedomain.org', 'sekretPa$$w0rd', function (err, newSubscription) {
-        subscription = newSubscription;
+      environment.addAccount('user@somedomain.org', 'sekretPa$$w0rd', function (err, newSubscriptions) {
+        subscriptions = newSubscriptions;
         done();
       });
     });
 
 
     it('should have called the token provider', function () {
-      environment.acquireToken.calledOnce.should.be.true;
+      environment.acquireToken.called.should.be.true;
     });
 
-    it('should pass expected configuration to tokn provider', function () {
+    it('should have listed subscriptions', function () {
+      environment.getAccountSubscriptions.called.should.be.true;
+    });
+
+    it('should pass expected configuration to token provider', function () {
       var config = environment.acquireToken.firstCall.args[0];
 
       config.should.have.properties({
         authorityUrl: environment.activeDirectoryEndpointUrl,
-        tenantId: 'somedomain.org',
+        tenantId: constants.DEFAULT_COMMON_ACTIVEDIRECTORY_TENANT_NAME,
         clientId: constants.XPLAT_CLI_CLIENT_ID,
         resourceId: constants.AZURE_MANAGEMENT_RESOURCE_ID
       });
     });
 
+    it('should pass token to get subscriptions', function() {
+      var token = environment.getAccountSubscriptions.firstCall.args[0];
+      token.should.equal(expectedToken.accessToken);
+    });
+
     it('should return a subscription with expected token', function () {
-      should.exist(subscription.accessToken);
-      subscription.accessToken.accessToken.should.equal('a dummy token');
+      should.exist(subscriptions[0].accessToken);
+      subscriptions[0].accessToken.accessToken.should.equal('a dummy token');
+    });
+
+    it('should return listed subscriptions', function () {
+      subscriptions.should.have.length(expectedSubscriptions.length);
+      for(var i = 0, len = subscriptions.length; i < len; ++i) {
+        subscriptions[i].id.should.equal(expectedSubscriptions[i].subscriptionId);
+        subscriptions[i].name.should.equal(expectedSubscriptions[i].subscriptionName);
+      }
+    });
+
+    it('should have same token for all subscription', function () {
+      subscriptions.forEach(function (s) {
+        s.accessToken.accessToken.should.equal(expectedToken.accessToken);
+      });
     });
   });
 });
