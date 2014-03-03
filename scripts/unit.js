27486ca93488a443fb87bdf9c8b3bf4ce2ae9c0c
Andre Rodrigues <andrerod@microsoft.com>
2014-03-03 14:30:23 +0000

    Uncomment code

--- scripts/unit.js
+++ scripts/unit.js
@@ -171,7 +171,7 @@ if (!process.env.NOCK_OFF) {
     if (process.env.AZURE_CERTIFICATE_FILE) {
       process.env.AZURE_CERTIFICATE = fs.readFileSync(process.env.AZURE_CERTIFICATE_FILE).toString();
     }
-/*
+
     if (!process.env.AZURE_CERTIFICATE) {
       throw new Error('Azure certificate needs to be defined for recordings');
     }
@@ -183,9 +183,10 @@ if (!process.env.NOCK_OFF) {
     if (!process.env.AZURE_CERTIFICATE_KEY) {
       throw new Error('Azure certificate key needs to be defined for recordings');
     }
+
     if (!process.env.AZURE_STORAGE_CONNECTION_STRING && !process.env.AZURE_STORAGE_ACCOUNT) {
       throw new Error('Azure storage connection string needs to be defined for recordings');
-    }*/
+    }
   }
 } else {
   if (mcOption) {
