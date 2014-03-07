6a1c3e73a20a448321cc3160eda1626c20a8152f
Chris Tavares <ctavares@microsoft.com>
2014-03-07 10:45:10 -0800

    Starting work on group show

--- test/commands/csm/groups/csm.group-template-tests.js
+++ test/commands/csm/groups/csm.group-template-tests.js
@@ -93,20 +93,23 @@ describe('csm', function () {
         });
       });
 
-      // describe('show', function () {
-      //   it('should show a resource group template from gallery', function (done) {
-      //     var templateName = 'Microsoft.WebSiteMySQLDatabase.0.1.0-preview1';
-      //     suite.execute('group template show %s --json --env %s', templateName, process.env['AZURE_CSM_TEST_ENVIRONMENT'], function (result) {
-      //       result.exitStatus.should.equal(0);
+      describe('show', function () {
+        it('should show a resource group template from gallery', function (done) {
+          var templateName = 'Microsoft.WebSiteMySQLDatabase.0.1.0-preview1';
+          suite.execute('group template show %s --json --env %s', templateName, process.env['AZURE_CSM_TEST_ENVIRONMENT'], function (result) {
+            result.exitStatus.should.equal(0);
 
-      //       var template = JSON.parse(result.text);
-      //       should.exist(template);
-      //       // template.identity.should.equal(templateName);
+            var template = JSON.parse(result.text);
+            should.exist(template);
+            template.identity.should.equal(templateName);
+            template.publisher.should.equal('Microsoft');
+            template.version.should.equal('0.1.0-preview1');
 
-      //       done();
-      //     });
-      //   });
-      // });
+            done();
+          });
+        });
+      });
+
     });
   });
 });
