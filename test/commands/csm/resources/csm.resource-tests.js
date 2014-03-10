55ed0d3d6d2437a97a2f4288b0356ea6d469acd2
yugangw-msft <yugangw@microsoft.com>
2014-03-10 10:41:35 -0700

    Fixes some typo and whitespaces

--- test/commands/csm/resources/csm.resource-tests.js
+++ test/commands/csm/resources/csm.resource-tests.js
@@ -251,13 +251,14 @@ describe('csm', function () {
           suite.execute('resource create %s %s %s %s -p %s --json', groupName, resourceName, 'Microsoft.Web/sites', testLocation, '{ "Name": "' + resourceName + '", "SiteMode": "Limited", "ComputeMode": "Shared" }', function (result) {
             result.exitStatus.should.equal(0);
 
-            //Make a change like set the SiteMode To Free
+            //Make a change like set the 'SiteMode' To 'Free'
             suite.execute('resource set %s %s %s %s -p %s --json', groupName, resourceName, 'Microsoft.Web/sites', testLocation, '{ "Name": "' + resourceName + '","SiteMode": "Free", "ComputeMode": "Shared" }', function (setResult) {
               setResult.exitStatus.should.equal(0);
 
               suite.execute('resource show %s %s %s --json', groupName, resourceName, 'Microsoft.Web/sites', function (showResult) {
                 showResult.exitStatus.should.equal(0);
 
+                //Serach for "siteMode":"Free" to make sure Set did work
                 showResult.text.indexOf('"siteMode": "Free",').should.be.above(-1);
 
                 suite.execute('group delete %s --quiet --json', groupName, function () {
