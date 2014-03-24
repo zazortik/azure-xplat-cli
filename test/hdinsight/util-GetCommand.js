8d1dd46ff12851698b14f49e42abe42f83bb4fcd
Chris Tavares <ctavares@microsoft.com>
2014-03-24 14:46:59 -0700

    Moved directories and changed mode names

--- test/hdinsight/util-GetCommand.js
+++ test/hdinsight/util-GetCommand.js
@@ -1,18 +1,18 @@
-// 
+//
 // Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 
 // Test includes
 var testutil = require('../util/util');
@@ -23,7 +23,7 @@ var ExecutionProcessorStub = require('./stub-executionProcessor.js');
 
 // Lib includes
 var util = testutil.libRequire('util/utils');
-var HDInsightCli = require('../../lib/commands/rdfe/hdinsight._js')
+var HDInsightCli = require('../../lib/commands/asm/hdinsight._js')
 
 function GetCommand() {
   this.cli = new CliStub();
