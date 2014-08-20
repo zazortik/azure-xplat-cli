2464641a77dcfd3bd7061c0e02b20bc2b0513fa7
Amar Zavery <amzavery@microsoft.com>
2014-08-20 10:39:00 -0700

    Initial commit for storing test output in a log file

---
+++ test/framework/azure-list-reporter.js
@@ -0,0 +1,69 @@
+
+/**
+ * Module dependencies.
+ */
+
+var testLogger = require('./test-logger');
+var Base = require('../../node_modules/mocha/lib/reporters/base')
+  , cursor = Base.cursor
+  , color = Base.color;
+
+/**
+ * Expose `List`.
+ */
+
+exports = module.exports = List;
+
+/**
+ * Initialize a new `List` test reporter.
+ *
+ * @param {Runner} runner
+ * @api public
+ */
+
+function List(runner) {
+  Base.call(this, runner);
+
+  var self = this
+    , stats = this.stats
+    , n = 0;
+
+  runner.on('start', function(){
+    console.log();
+  });
+
+  runner.on('test', function(test){
+    process.stdout.write(color('pass', '    ' + test.fullTitle() + ': '));
+    testLogger.logData('>>>>>>>>>> ' + test.fullTitle() + ': >>>>>>>>>>');
+  });
+
+  runner.on('pending', function(test){
+    var fmt = color('checkmark', '  -')
+      + color('pending', ' %s');
+    console.log(fmt, test.fullTitle());
+    testLogger.logData('-----skipped----- ' + test.fullTitle());
+  });
+
+  runner.on('pass', function(test){
+    var fmt = color('checkmark', '  '+Base.symbols.dot)
+      + color('pass', ' %s: ')
+      + color(test.speed, '%dms');
+    cursor.CR();
+    console.log(fmt, test.fullTitle(), test.duration);
+    testLogger.logData('########## ' + test.fullTitle() + ': ' + test.duration + 'ms  ##########');
+  });
+
+  runner.on('fail', function(test, err){
+    cursor.CR();
+    console.log(color('fail', '  %d) %s'), ++n, test.fullTitle());
+    testLogger.logData(n + '  ' + test.fullTitle());
+  });
+
+  runner.on('end', self.epilogue.bind(self));
+}
+
+/**
+ * Inherit from `Base.prototype`.
+ */
+
+List.prototype.__proto__ = Base.prototype;
