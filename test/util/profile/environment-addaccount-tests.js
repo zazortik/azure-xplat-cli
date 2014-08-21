0fe263ff6471c3d713faf08ee8e3a978b4b0ba67
yugangw-msft <yugangw@microsoft.com>
2014-08-21 15:27:51 -0700

    do not call rdfe subscription client

--- test/util/profile/environment-addaccount-tests.js
+++ test/util/profile/environment-addaccount-tests.js
@@ -18,7 +18,7 @@
 var _ = require('underscore');
 var should = require('should');
 var sinon = require('sinon');
-require('streamline').register()
+require('streamline').register();
 
 var constants = require('../../../lib/util/constants');
 var profile = require('../../../lib/util/profile');
@@ -27,44 +27,32 @@ var subscriptionUtils = require('../../../lib/util/profile/subscriptionUtils._js
 var expectedUserName = 'user@somedomain.example';
 var expectedPassword = 'sekretPa$$w0rd';
 
+var testTenantIds = ['2d006e8c-61e7-4cd2-8804-b4177a4341a1'];
+
 var expectedSubscriptions = 
 [
   {
     subscriptionId: 'db1ab6f0-4769-4b27-930e-01e2ef9c123c',
-    subscriptionName: 'Account',
-    username: expectedUserName
+    displayName: 'Account',
+    username: expectedUserName,
+    activeDirectoryTenantId: testTenantIds[0]
   },
   {
     subscriptionId: 'db1ab6f0-4769-4b27-930e-01e2ef9c124d',
-    subscriptionName: 'Other',
-    username: expectedUserName
+    displayName: 'Other',
+    username: expectedUserName,
+    activeDirectoryTenantId: testTenantIds[0]
   },
-  {
-    subscriptionId: 'db1ab6f0-4769-4b27-930e-01e2ef9c125e',
-    subscriptionName: 'External',
-    username: expectedUserName
-  }
 ];
 
-var testAsmSubscriptions = [
+var testSubscriptionsFromTenant = [
   {
     subscriptionId: 'db1ab6f0-4769-4b27-930e-01e2ef9c123c',
-    subscriptionName: 'Account'
+    displayName: 'Account'
   },
   {
     subscriptionId: 'db1ab6f0-4769-4b27-930e-01e2ef9c124d',
-    subscriptionName: 'Other'
-  }
-];
-
-var testArmSubscriptions = [
-  {
-    subscriptionId: 'db1ab6f0-4769-4b27-930e-01e2ef9c125e', // a new one, arm specific
-    displayName: 'External'
-  },
-  {
-    subscriptionId: 'db1ab6f0-4769-4b27-930e-01e2ef9c123c', //this is the duplicate with asm one
-    displayName: 'Account' 
+    displayName: 'Other'
   }
 ];
 
@@ -75,20 +63,11 @@ var expectedToken = {
   authenticateRequest: function () { }
 };
 
-var testAsmSubscriptionClient = {
-  subscriptions: {
-    list: function (callback) {
-      callback(null, {subscriptions: testAsmSubscriptions });
-    }
-  }
-};
-
-var testTenantIds = ['2d006e8c-61e7-4cd2-8804-b4177a4341a1'];
 
 var testArmSubscriptionClient = {
   subscriptions: {
     list: function (callback) {
-      callback(null, {subscriptions: testArmSubscriptions });
+      callback(null, {subscriptions: testSubscriptionsFromTenant });
     }
   },
   tenants: {
@@ -115,7 +94,6 @@ describe('Environment', function () {
     
     sinon.stub(environment, 'acquireToken').callsArgWith(3/*4th parameter of 'acquireToken' is the callback*/, 
       null/*no error*/, expectedToken/*the access token*/);
-    sinon.stub(environment, 'getAsmClient').returns(testAsmSubscriptionClient);
     sinon.stub(environment, 'getArmClient').returns(testArmSubscriptionClient);
   });
 
@@ -132,10 +110,6 @@ describe('Environment', function () {
     it('should have called the token provider', function () {
       environment.acquireToken.called.should.be.true;
     });
-
-    it('should have call to get asm client', function () {
-      environment.getAsmClient.called.should.be.true;
-    });
     
     it('should have call to get arm client', function () {
       environment.getArmClient.called.should.be.true;
@@ -148,11 +122,11 @@ describe('Environment', function () {
       var password = environment.acquireToken.firstCall.args[1];
       password.should.equal(expectedPassword);
 
-      var tenantId_1 = environment.acquireToken.firstCall.args[2];
-      tenantId_1.should.equal(''); // '' mean using the common tenant
+      var tenantId1 = environment.acquireToken.firstCall.args[2];
+      tenantId1.should.equal(''); // '' mean using the common tenant
 
-      var tenantId_2 = environment.acquireToken.secondCall.args[2];
-      tenantId_2.should.equal(testTenantIds[0]);
+      var tenantId2 = environment.acquireToken.secondCall.args[2];
+      tenantId2.should.equal(testTenantIds[0]);
     });
 
     it('should return a subscription with expected username', function () {
@@ -164,13 +138,19 @@ describe('Environment', function () {
       subscriptions.should.have.length(expectedSubscriptions.length);
       for(var i = 0, len = subscriptions.length; i < len; ++i) {
         subscriptions[i].id.should.equal(expectedSubscriptions[i].subscriptionId);
-        subscriptions[i].name.should.equal(expectedSubscriptions[i].subscriptionName);
+        subscriptions[i].name.should.equal(expectedSubscriptions[i].displayName);
       }
     });
 
     it('should have same username for all subscription', function () {
       subscriptions.forEach(function (s) {
         s.username.should.equal(expectedUserName);
+      });
+    });
+
+    it('should have same tenant id for all subscription', function () {
+      subscriptions.forEach(function (s) {
+        s.tenantId.should.equal(testTenantIds[0]);
       });
     });
   });
