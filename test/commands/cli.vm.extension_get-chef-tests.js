8875820358fd62d3b8d2425e230c614b7b26141e
siddheshwar-more <siddheshwar.more@clogeny.com>
2015-02-02 21:39:45 +0530

    Added unit tests for set/get chef extension commands

---
+++ test/commands/cli.vm.extension_get-chef-tests.js
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) Microsoft.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var should = require('should');
+var util = require('util');
+var testUtils = require('../util/util');
+var CLITest = require('../framework/cli-test');
+
+// A common VM used by multiple tests
+var suite;
+var testPrefix = 'cli.vm.extension_get-chef-tests';
+
+var requiredEnvironment = [{
+  name: 'AZURE_VM_TEST_LOCATION',
+  defaultValue: 'West US'
+}];
+
+describe('cli', function() {
+  describe('vm', function() {
+    var extensionList,
+      exteAylist,
+      location,
+      retry = 5;
+    testUtils.TIMEOUT_INTERVAL = 5000;
+
+    before(function(done) {
+      suite = new CLITest(testPrefix, requiredEnvironment);
+      suite.setupSuite(done);
+    });
+
+    after(function(done) {
+      suite.teardownSuite(done);
+    });
+
+    beforeEach(function(done) {
+      suite.setupTest(function() {
+        location = process.env.AZURE_VM_TEST_LOCATION;
+        timeout = suite.isMocked ? 0 : testUtils.TIMEOUT_INTERVAL;
+        done();
+      });
+    });
+
+    afterEach(function(done) {
+      suite.teardownTest(done);
+    });
+
+    describe('Extension:', function() {
+
+      it('get the details of VM', function(done) {
+        getVM(function(vmName) {
+          var cmd = util.format('vm extension get %s --json', vmName).split(' ');
+          testUtils.executeCommand(suite, retry, cmd, function(result) {
+            result.exitStatus.should.equal(0);
+            extensionList = JSON.parse(result.text);
+            extensionList.length.should.be.above(0);
+            exteAylist = extensionList.filter(function(ex){ return ex.publisher  == 'Chef.Bootstrap.WindowsAzure'; })[0]
+            done();
+          });
+        });
+      });
+
+      //Get Complete extension output
+      it('get chef extension output', function(done) {
+        getVM(function(vmName) {
+          var cmd = util.format('vm extension get-chef --json', vmName).split(' ');
+          testUtils.executeCommand(suite, retry, cmd, function(result) {
+            result.exitStatus.should.equal(0);
+            var ext = JSON.parse(result.text);
+            ext.length.should.be.above(0);
+            ext[0].name.should.equal(exteAylist.name);
+            ext[0].publisher.should.equal(exteAylist.publisher);
+            ext[0].referenceName.should.equal(exteAylist.referenceName);
+            done();
+          });
+        });
+      });
+    });
+
+    function getVM(callback) {
+      if (getVM.VMName) {
+        callback(getVM.VMName);
+      } else {
+        var cmd = util.format('vm list --json').split(' ');
+        testUtils.executeCommand(suite, retry, cmd, function(result) {
+          result.exitStatus.should.equal(0);
+          var vmList = JSON.parse(result.text);
+          var found = vmList.some(function(vm) {
+            if (vm.OSDisk.operatingSystem.toLowerCase() === 'windows') {
+              getVM.VMName = vm.VMName;
+              return true;
+            }
+          });
+          callback(getVM.VMName);
+        });
+      }
+    }
+  });
+});
