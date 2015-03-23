e4ad830e6c9e68f3a68071c8b8ea281e9a059d7d
Amar Zavery <amzavery@microsoft.com>
2015-03-23 15:14:54 -0700

    recorded static vm create-from tests

--- test/recordings/cli.vm.staticvm_create-from-tests/suite.cli.vm.staticvm_create-from-tests.nock.js
+++ test/recordings/cli.vm.staticvm_create-from-tests/suite.cli.vm.staticvm_create-from-tests.nock.js
@@ -7,17 +7,13 @@ exports.getMockedProfile = function () {
 
   newProfile.addSubscription(new profile.Subscription({
     id: '2c224e7e-3ef5-431d-a57b-e71f4662e3a6',
-    managementCertificate: {
-      key: 'mockedKey',
-      cert: 'mockedCert'
-    },
     name: 'Node CLI Test',
     user: {
       name: 'user@domain.example',
       type: 'user'
     },
     tenantId: '72f988bf-86f1-41af-91ab-2d7cd011db47',
-    registeredProviders: ['website', 'website'],
+    registeredProviders: ['website'],
     registeredResourceNamespaces: [],
     isDefault: true
   }, newProfile.environments['AzureCloud']));
@@ -31,4 +27,4 @@ exports.setEnvironment = function() {
 
 exports.scopes = [];
 
- exports.randomTestIdsGenerated = function() { return ['clitestvm8091'];};
+ exports.randomTestIdsGenerated = function() { return ['clitestvm3018'];};
