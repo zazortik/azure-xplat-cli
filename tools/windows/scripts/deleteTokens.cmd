f44873b64bcebe36b0a641381de1a3a54332dd60
yugangw-msft <yugangw@microsoft.com>
2015-07-13 13:37:55 -0700

    update comments

--- tools/windows/scripts/deleteTokens.cmd
+++ tools/windows/scripts/deleteTokens.cmd
@@ -1,7 +1,7 @@
 ::
-:: Microsoft Azure CLI - scripts to disable tracing through fiddler
+:: Microsoft Azure CLI - delete all cli's access tokens saved 
+:: in the local security store.
 :: Copyright (C) Microsoft Corporation. All Rights Reserved.
 ::
 
-::delete all cli's access tokens saved in the local security store.
 ..\..\..\bin\windows\creds.exe -d -t AzureXplatCli:target=* -g
