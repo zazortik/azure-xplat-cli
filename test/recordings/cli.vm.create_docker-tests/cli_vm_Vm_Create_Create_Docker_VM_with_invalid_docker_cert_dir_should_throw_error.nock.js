b027825eb759cc091f90649bcfc5a37d26cd1a0d
sreekanthbs <sreekanthb@brillio.com>
2015-07-23 12:58:50 +0530

    ASM Docker recordings modified
    ASM Docker recordings modified

--- test/recordings/cli.vm.create_docker-tests/cli_vm_Vm_Create_Create_Docker_VM_with_invalid_docker_cert_dir_should_throw_error.nock.js
+++ test/recordings/cli.vm.create_docker-tests/cli_vm_Vm_Create_Create_Docker_VM_with_invalid_docker_cert_dir_should_throw_error.nock.js
@@ -25,4 +25,4 @@ exports.setEnvironment = function() {
 };
 
 exports.scopes = [[]];
- exports.randomTestIdsGenerated = function() { return ['clitestvm7074'];};
+ exports.randomTestIdsGenerated = function() { return ['clitestvm7697'];};
--- test/recordings/cli.vm.create_docker-tests/cli_vm_Vm_Create_Create_Docker_VM_with_ssh_cert_and_no_ssh_password_but_no_ssh_endpoint_or_explicit_disabled_ssh_endpoint_should_throw_error.nock.js
+++ test/recordings/cli.vm.create_docker-tests/cli_vm_Vm_Create_Create_Docker_VM_with_ssh_cert_and_no_ssh_password_but_no_ssh_endpoint_or_explicit_disabled_ssh_endpoint_should_throw_error.nock.js
@@ -25,4 +25,4 @@ exports.setEnvironment = function() {
 };
 
 exports.scopes = [[]];
- exports.randomTestIdsGenerated = function() { return ['clitestvm757'];};
+ exports.randomTestIdsGenerated = function() { return ['clitestvm9159'];};
