6f71927c78a3e127b120803657313a32e49cee8f
Amar Zavery <amzavery@microsoft.com>
2015-09-11 10:51:49 -0700

    recorded template download tests

--- test/recordings/arm-cli-group-templates-tests/suite.arm-cli-group-templates-tests.nock.js
+++ test/recordings/arm-cli-group-templates-tests/suite.arm-cli-group-templates-tests.nock.js
@@ -6,8 +6,8 @@ exports.getMockedProfile = function () {
   var newProfile = new profile.Profile();
 
   newProfile.addSubscription(new profile.Subscription({
-    id: '38b598fc-e57a-423f-b2e7-dc0ddb631f1f',
-    name: 'Visual Studio Ultimate with MSDN',
+    id: '00977cdb-163f-435f-9c32-39ec8ae61f4d',
+    name: 'node',
     user: {
       name: 'user@domain.example',
       type: 'user'
@@ -21,7 +21,7 @@ exports.getMockedProfile = function () {
 };
 
 exports.setEnvironment = function() {
-  process.env['AZURE_ARM_TEST_LOCATION'] = 'West US';
+  process.env['AZURE_ARM_TEST_LOCATION'] = 'South Central US';
 };
 
 exports.scopes = [];
