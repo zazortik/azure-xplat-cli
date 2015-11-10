9605e7937220636b023a953e78b5f4e61fa9d074
huangpf <phuang@microsoft.com>
2015-11-10 09:04:16 -0800

    Address CR

--- test/commands/cli.service.cert-tests.js
+++ test/commands/cli.service.cert-tests.js
@@ -12,8 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 var should = require('should');
-var util = require('util');
 var testUtils = require('../util/util');
 var CLITest = require('../framework/cli-test');
 
@@ -31,87 +31,87 @@ var requiredEnvironment = [
 ];
 
 describe('cli', function() {
-    describe('service', function() {
-        var location, timeout, retry = 5;
-        testUtils.TIMEOUT_INTERVAL = 10000;
+  describe('service', function() {
+    var location, timeout, retry = 5;
+    testUtils.TIMEOUT_INTERVAL = 10000;
 
-        before(function(done) {
-            suite = new CLITest(this, testPrefix, requiredEnvironment);
-            suite.setupSuite(function() {
-                svcName = suite.generateId(svcPrefix, svcNames);
-                timeout = suite.isPlayback() ? 0 : testUtils.TIMEOUT_INTERVAL;
-                location = process.env.AZURE_CLOUD_SERVICE_TEST_LOCATION;
-                done();
-            });
-        });
-
-        after(function(done) {
-            suite.teardownSuite(done);
-        });
-
-        beforeEach(function(done) {
-            suite.setupTest(done);
-        });
-
-        afterEach(function(done) {
-            suite.teardownTest(done);
-        });
-
-        //create a image
-        describe('certificate:', function() {
-
-            it('create service', function(done) {
-                suite.execute('node cli.js service create --serviceName %s --location %s --json', svcName, location, function (result) {
-                    result.exitStatus.should.equal(0);
-                    done();
-                });
-            });
-
-            it('create certificate', function(done) {
-                suite.execute('node cli.js service cert create %s %s %s --json', svcName, certFile, password, function (result) {
-                    result.exitStatus.should.equal(0);
-                    done();
-                });
-            });
-
-            it('list certificates', function(done) {
-                suite.execute('node cli.js service cert list --json', function (result) {
-                    result.exitStatus.should.equal(0);
-                    done();
-                });
-            });
-
-            it('list service certificates', function(done) {
-                suite.execute('node cli.js service cert list --serviceName %s --json', svcName, function (result) {
-                    result.exitStatus.should.equal(0);
-                    var certs = JSON.parse(result.text);
-                    thumbprint = certs[0].thumbprint;
-                    thumbprintAlgorithm = certs[0].thumbprintAlgorithm;
-                    done();
-                });
-            });
-            
-            it('get certificate', function(done) {
-                suite.execute('node cli.js service cert get --service-name %s --thumbprint %s --thumbprint-algorithm %s --json', svcName, thumbprint, thumbprintAlgorithm, function (result) {
-                    result.exitStatus.should.equal(0);
-                    done();
-                });
-            });
-
-            it('delete', function(done) {
-                suite.execute('node cli.js service cert delete %s %s --json', svcName, thumbprint, function (result) {
-                    result.exitStatus.should.equal(0);
-                    done();
-                });
-            });
-
-            it('delete service', function(done) {
-                suite.execute('node cli.js service delete %s -q --json', svcName, function (result) {
-                    result.exitStatus.should.equal(0);
-                    done();
-                });
-            });
-        });
-
+    before(function(done) {
+      suite = new CLITest(this, testPrefix, requiredEnvironment);
+      suite.setupSuite(function() {
+        svcName = suite.generateId(svcPrefix, svcNames);
+        timeout = suite.isPlayback() ? 0 : testUtils.TIMEOUT_INTERVAL;
+        location = process.env.AZURE_CLOUD_SERVICE_TEST_LOCATION;
+        done();
+      });
     });
+
+    after(function(done) {
+      suite.teardownSuite(done);
+    });
+
+    beforeEach(function(done) {
+      suite.setupTest(done);
+    });
+
+    afterEach(function(done) {
+      suite.teardownTest(done);
+    });
+
+    //create a image
+    describe('certificate:', function() {
+
+      it('create service', function(done) {
+        suite.execute('node cli.js service create --serviceName %s --location %s --json', svcName, location, function (result) {
+          result.exitStatus.should.equal(0);
+          done();
+        });
+      });
+
+      it('create certificate', function(done) {
+        suite.execute('node cli.js service cert create %s %s %s --json', svcName, certFile, password, function (result) {
+          result.exitStatus.should.equal(0);
+          done();
+        });
+      });
+
+      it('list certificates', function(done) {
+        suite.execute('node cli.js service cert list --json', function (result) {
+          result.exitStatus.should.equal(0);
+          done();
+        });
+      });
+
+      it('list service certificates', function(done) {
+        suite.execute('node cli.js service cert list --serviceName %s --json', svcName, function (result) {
+          result.exitStatus.should.equal(0);
+          var certs = JSON.parse(result.text);
+          thumbprint = certs[0].thumbprint;
+          thumbprintAlgorithm = certs[0].thumbprintAlgorithm;
+          done();
+        });
+      });
+
+      it('get certificate', function(done) {
+        suite.execute('node cli.js service cert get --service-name %s --thumbprint %s --thumbprint-algorithm %s --json', svcName, thumbprint, thumbprintAlgorithm, function (result) {
+          result.exitStatus.should.equal(0);
+          done();
+        });
+      });
+
+      it('delete', function(done) {
+        suite.execute('node cli.js service cert delete %s %s --json', svcName, thumbprint, function (result) {
+          result.exitStatus.should.equal(0);
+          done();
+        });
+      });
+
+      it('delete service', function(done) {
+        suite.execute('node cli.js service delete %s -q --json', svcName, function (result) {
+          result.exitStatus.should.equal(0);
+          done();
+        });
+      });
+    });
+
+  });
 });
