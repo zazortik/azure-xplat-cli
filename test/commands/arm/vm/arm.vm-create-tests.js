87954a1483db87ae9758851661dcb965f2f10514
huangpf <phuang@microsoft.com>
2015-11-30 13:53:13 -0800

    Expand VM Create Test Time-out

--- test/commands/arm/vm/arm.vm-create-tests.js
+++ test/commands/arm/vm/arm.vm-create-tests.js
@@ -94,6 +94,7 @@ describe('arm', function() {
       });
     });
     after(function(done) {
+      this.timeout(vmTest.timeoutLarge * 10);
       vmTest.deleteUsedGroup(groupName, suite, function(result) {
         suite.teardownSuite(done);
       });
@@ -308,7 +309,7 @@ describe('arm', function() {
       });
 
       it('delete should delete VM', function(done) {
-        this.timeout(vmTest.timeoutLarge);
+        this.timeout(vmTest.timeoutLarge * 10);
         var cmd = util.format('vm delete %s %s --quiet --json', groupName, vmPrefix).split(' ');
         testUtils.executeCommand(suite, retry, cmd, function(result) {
           result.exitStatus.should.equal(0);
