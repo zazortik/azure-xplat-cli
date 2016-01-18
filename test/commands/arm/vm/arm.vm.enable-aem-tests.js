4954503ebdf62ae92ffbcb706a3241cc3932966c
Ubuntu <zhongyi@zhongyi000.o0j1lcctmvmefkmde2zjemxuae.bx.internal.cloudapp.net>
2016-01-18 04:51:52 +0000

    fix aem tests

--- test/commands/arm/vm/arm.vm.enable-aem-tests.js
+++ test/commands/arm/vm/arm.vm.enable-aem-tests.js
@@ -161,9 +161,9 @@ describe('arm', function() {
         testUtils.executeCommand(suite, retry, cmd, function(result) {
           result.exitStatus.should.equal(0);
           var allResources = JSON.parse(result.text);
-          allResources[1].publisher.should.equal(publisherExt);
-          allResources[1].name.should.equal(extension);
-          allResources[1].typeHandlerVersion.should.equal(version);
+          allResources[0].publisher.should.equal(publisherExt);
+          allResources[0].name.should.equal(extension);
+          allResources[0].typeHandlerVersion.should.equal(version);
           done();
         });
       });
