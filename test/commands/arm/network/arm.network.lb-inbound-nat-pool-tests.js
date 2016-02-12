838222ee58a71decf3ff00de727bbc5c94b2e5de
romangromov <kinoshnik.rom@gmail.com>
2016-02-12 14:05:56 +0300

    ARM: nat pool tests improved

--- test/commands/arm/network/arm.network.lb-inbound-nat-pool-tests.js
+++ test/commands/arm/network/arm.network.lb-inbound-nat-pool-tests.js
@@ -12,29 +12,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 'use strict';
 
 var should = require('should');
 var util = require('util');
+var _ = require('underscore');
 var testUtils = require('../../../util/util');
 var CLITest = require('../../../framework/arm-cli-test');
-var testprefix = 'arm-network-lb-inbound-nat-pool-tests';
 var networkTestUtil = require('../../../util/networkTestUtil');
-var groupName,
-  location,
-  groupPrefix = 'xplatTestGCreateLbNatPool',
-  publicipPrefix = 'xplatTestIpLbNatPool',
-  LBName = 'xplattestLbLbNatPool',
-  FrontendIpName = 'xplattestFrontendIpNatPool',
-  lbinboundnatpoolprefix = 'xplattestInboundNatPool';
-var protocol = 'tcp',
-  frontendportrangestart = '11',
-  frontendportrangeend = '21',
-  backendport = '3380';
-var protocol2 = 'udp',
-  frontendportrangestart2 = '31',
-  frontendportrangeend2 = '41',
-  backendport2 = '3381';
+
+var testPrefix = 'arm-network-lb-inbound-nat-pool-tests',
+  groupName = 'xplat-test-lb-inbound-nat-pools',
+  location;
+
+var poolProp = {
+  name: 'test-inbound-pool',
+  protocol: 'Tcp',
+  newProtocol: 'Udp',
+  frontendPortRangeStart: 50000,
+  newFrontendPortRangeStart: 50001,
+  frontendPortRangeEnd: 55000,
+  newFrontendPortRangeEnd: 55001,
+  backendPort: 80,
+  newBackendPort: 81
+};
+
+var lbName = 'test-lb',
+  publicIpName = 'test-ip',
+  fipName = 'test-fip';
+
 var requiredEnvironment = [{
   name: 'AZURE_VM_TEST_LOCATION',
   defaultValue: 'southeastasia'
@@ -46,24 +53,23 @@ describe('arm', function () {
       retry = 5;
     var networkUtil = new networkTestUtil();
     before(function (done) {
-      suite = new CLITest(this, testprefix, requiredEnvironment);
+      suite = new CLITest(this, testPrefix, requiredEnvironment);
       suite.setupSuite(function () {
         location = process.env.AZURE_VM_TEST_LOCATION;
-        groupName = suite.isMocked ? groupPrefix : suite.generateId(groupPrefix, null);
-        publicipPrefix = suite.isMocked ? publicipPrefix : suite.generateId(publicipPrefix, null);
-        LBName = suite.isMocked ? LBName : suite.generateId(LBName, null);
-        lbinboundnatpoolprefix = suite.isMocked ? lbinboundnatpoolprefix : suite.generateId(lbinboundnatpoolprefix, null);
-        FrontendIpName = suite.isMocked ? FrontendIpName : suite.generateId(FrontendIpName, null);
+        groupName = suite.isMocked ? groupName : suite.generateId(groupName, null);
+        lbName = suite.isMocked ? lbName : suite.generateId(lbName, null);
+        publicIpName = suite.isMocked ? publicIpName : suite.generateId(publicIpName, null);
+        fipName = suite.isMocked ? fipName : suite.generateId(fipName, null);
+
+        poolProp.group = groupName;
+        poolProp.lbName = lbName;
+        poolProp.name = suite.isMocked ? poolProp.name : suite.generateId(poolProp.name, null);
         done();
       });
     });
     after(function (done) {
-      networkUtil.deleteLB(groupName, LBName, suite, function () {
-        networkUtil.deletePublicIp(groupName, publicipPrefix, suite, function () {
-          networkUtil.deleteGroup(groupName, suite, function () {
-            suite.teardownSuite(done);
-          });
-        });
+      networkUtil.deleteGroup(groupName, suite, function () {
+        suite.teardownSuite(done);
       });
     });
     beforeEach(function (done) {
@@ -74,50 +80,72 @@ describe('arm', function () {
     });
 
     describe('lb-inbound-nat-pool', function () {
-
-      it('create should pass', function (done) {
+      it('create should create inbound nat pool in load balancer', function (done) {
         networkUtil.createGroup(groupName, location, suite, function () {
-          networkUtil.createLB(groupName, LBName, location, suite, function () {
-            networkUtil.createPublicIp(groupName, publicipPrefix, location, suite, function () {
-              networkUtil.showPublicIp(groupName, publicipPrefix, suite, function () {
-                networkUtil.createFIP(groupName, LBName, FrontendIpName, networkTestUtil.publicIpId, suite, function () {
-                  var cmd = util.format('network lb inbound-nat-pool create %s %s %s -p %s -f %s -e %s -b %s -i %s --json', groupName, LBName, lbinboundnatpoolprefix, protocol, frontendportrangestart, frontendportrangeend, backendport, FrontendIpName).split(' ');
-                  testUtils.executeCommand(suite, retry, cmd, function (result) {
-                    result.exitStatus.should.equal(0);
-                    done();
-                  });
+          networkUtil.createLB(groupName, lbName, location, suite, function () {
+            networkUtil.createPublicIp(groupName, publicIpName, location, suite, function (publicIp) {
+              networkUtil.createFIP(groupName, lbName, fipName, publicIp.id, suite, function (fip) {
+                var cmd = 'network lb inbound-nat-pool create -g {group} -l {lbName} -n {name} -p {protocol} -f {frontendPortRangeStart} -e {frontendPortRangeEnd} -b {backendPort} -i {1} --json'
+                  .formatArgs(poolProp, fip.name);
+
+                testUtils.executeCommand(suite, retry, cmd, function (result) {
+                  result.exitStatus.should.equal(0);
+                  var pool = JSON.parse(result.text);
+                  pool.name.should.equal(poolProp.name);
+                  pool.protocol.should.equal(poolProp.protocol);
+                  pool.frontendPortRangeStart.should.equal(poolProp.frontendPortRangeStart);
+                  pool.frontendPortRangeEnd.should.equal(poolProp.frontendPortRangeEnd);
+                  pool.backendPort.should.equal(poolProp.backendPort);
+                  pool.frontendIPConfiguration.id.should.equal(fip.id);
+                  networkUtil.shouldBeSucceeded(pool);
+                  done();
                 });
               });
             });
           });
         });
       });
-
+      it('set should modify inbound nat pool in load balancer', function (done) {
+        var cmd = 'network lb inbound-nat-pool set -g {group} -l {lbName} -n {name} -p {newProtocol} -f {newFrontendPortRangeStart} -e {newFrontendPortRangeEnd} -b {newBackendPort} --json'.formatArgs(poolProp);
+        testUtils.executeCommand(suite, retry, cmd, function (result) {
+          result.exitStatus.should.equal(0);
+          var pool = JSON.parse(result.text);
+          pool.name.should.equal(poolProp.name);
+          pool.protocol.should.equal(poolProp.newProtocol);
+          pool.frontendPortRangeStart.should.equal(poolProp.newFrontendPortRangeStart);
+          pool.frontendPortRangeEnd.should.equal(poolProp.newFrontendPortRangeEnd);
+          pool.backendPort.should.equal(poolProp.newBackendPort);
+          networkUtil.shouldBeSucceeded(pool);
+          done();
+        });
+      });
       it('list should display all inbound-nat-pool in load balancer', function (done) {
-        var cmd = util.format('network lb inbound-nat-pool list %s %s --json', groupName, LBName).split(' ');
+        var cmd = 'network lb inbound-nat-pool list -g {group} -l {lbName} --json'.formatArgs(poolProp);
         testUtils.executeCommand(suite, retry, cmd, function (result) {
           result.exitStatus.should.equal(0);
-          var allResources = JSON.parse(result.text);
-          allResources[0].name.should.equal(lbinboundnatpoolprefix);
+          var pools = JSON.parse(result.text);
+          _.some(pools, function (pool) {
+            return pool.name === poolProp.name;
+          }).should.be.true;
           done();
         });
       });
-      it('set should modify inbound-nat-pool', function (done) {
-        var cmd = util.format('network lb inbound-nat-pool set %s %s %s -p %s -f %s -e %s -b %s -i %s --json', groupName, LBName, lbinboundnatpoolprefix, protocol2, frontendportrangestart2, frontendportrangeend2, backendport2, FrontendIpName).split(' ');
+      it('delete should delete inbound-nat-pool in load balancer', function (done) {
+        var cmd = 'network lb inbound-nat-pool delete -g {group} -l {lbName} -n {name} --quiet --json'.formatArgs(poolProp);
         testUtils.executeCommand(suite, retry, cmd, function (result) {
           result.exitStatus.should.equal(0);
-          done();
-        });
-      });
-      it('delete should delete inbound-nat-pool', function (done) {
-        var cmd = util.format('network lb inbound-nat-pool delete %s %s %s --quiet --json', groupName, LBName, lbinboundnatpoolprefix).split(' ');
-        testUtils.executeCommand(suite, retry, cmd, function (result) {
-          result.exitStatus.should.equal(0);
-          done();
-        });
-      });
 
+          cmd = 'network lb inbound-nat-pool list -g {group} -l {lbName} --json'.formatArgs(poolProp);
+          testUtils.executeCommand(suite, retry, cmd, function (result) {
+            result.exitStatus.should.equal(0);
+            var pools = JSON.parse(result.text);
+            _.some(pools, function (pool) {
+              return pool.name === poolProp.name;
+            }).should.be.false;
+            done();
+          });
+        });
+      });
     });
-
   });
 });
