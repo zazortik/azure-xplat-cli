a4489efd930943357412ac196df5bcb3f36cb37e
Amar Zavery <amzavery@microsoft.com>
2016-04-20 13:59:19 -0700

    fixes #2136.

--- test/recordings/cli.service.cert-tests/suite.cli.service.cert-tests.nock.js
+++ test/recordings/cli.service.cert-tests/suite.cli.service.cert-tests.nock.js
@@ -6,8 +6,8 @@ exports.getMockedProfile = function () {
   var newProfile = new profile.Profile();
 
   newProfile.addSubscription(new profile.Subscription({
-    id: '4d368445-cbb1-42a7-97a6-6850ab99f48e',
-    name: 'Internal Consumption',
+    id: '00977cdb-163f-435f-9c32-39ec8ae61f4d',
+    name: 'node',
     user: {
       name: 'user@domain.example',
       type: 'user'
@@ -15,6 +15,7 @@ exports.getMockedProfile = function () {
     tenantId: '72f988bf-86f1-41af-91ab-2d7cd011db47',
     state: 'Enabled',
     registeredProviders: [],
+    _eventsCount: '1',
     isDefault: true
   }, newProfile.environments['AzureCloud']));
 
@@ -27,4 +28,4 @@ exports.setEnvironment = function() {
 
 exports.scopes = [];
 
- exports.randomTestIdsGenerated = function() { return ['clitestcert9491'];};
+ exports.randomTestIdsGenerated = function() { return ['clitestcert4113'];};
