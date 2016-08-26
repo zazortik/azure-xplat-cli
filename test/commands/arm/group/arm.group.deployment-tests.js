2fee20a249ad2ceeba8a50da503059f084a848ac
Andrew Mestas (InConsulting Inc.) <v-anmest@microsoft.com>
2016-08-26 16:13:59 -0700

    Template Changes for prompt of missing parameters and auto generate name from SAS token in URI

--- test/commands/arm/group/arm.group.deployment-tests.js
+++ test/commands/arm/group/arm.group.deployment-tests.js
@@ -22,7 +22,7 @@ var fs = require('fs');
 var path = require('path');
 var CLITest = require('../../../framework/arm-cli-test');
 var testUtil = require('../../../util/util');
-
+var jsonminify = require('jsonminify');
 var requiredEnvironment = [
   { requiresToken: true },
   { name: 'AZURE_ARM_TEST_LOCATION', defaultValue: 'West US' }
@@ -85,9 +85,15 @@ describe('arm', function () {
       });
     }
     
-    function setUniqParameterNames(suite, filename) {
-      //no need to create unique parameter values in playbackmode
-      var deploymentParameters = JSON.parse(fs.readFileSync(filename).toString());
+    function setUniqParameterNames(suite, filename, writeComment) {
+            //no need to create unique parameter values in playbackmode
+      if(writeComment){
+        fs.appendFile(filename, '//Single Line Comment \n /*A \n Multiline \n Comment */' , function(err) {
+            if (err) throw err;
+                console.log('Added single and multiline comments to file ', filename);
+            });
+      }
+      var deploymentParameters = JSON.parse(JSON.minify(fs.readFileSync(filename).toString()));
       if (deploymentParameters.parameters) {
         deploymentParameters = deploymentParameters.parameters;
       }
@@ -105,6 +111,14 @@ describe('arm', function () {
       if (!suite.isPlayback()) {
         fs.writeFileSync(filename, JSON.stringify(deploymentParameters, null, 2));
       }
+    }
+        
+    function thirtyMinutesFromNow(){
+      var date = new Date();
+      date.setMinutes(date.getMinutes() + 30);
+      var expireTime = date.toISOString().split(":").splice(0, 2);
+      expireTime = expireTime.join(":") + '-07:00';
+      return expireTime;
     }
 
     describe('list and show', function () {
@@ -262,12 +276,129 @@ describe('arm', function () {
             
             suite.execute('group deployment stop -g %s -n %s -q --json', groupName, deploymentName, function (listResult) {
               listResult.exitStatus.should.equal(0);
-              
               cleanup(done);
             });
           });
         });
       });
+      it('should work with URI containing SAS token', function (done) {
+        var parameterFile = path.join(__dirname, '../../../data/startersite-parameters.json');
+        setUniqParameterNames(suite, parameterFile);
+        var groupName = suite.generateId('xDeploymentTestGroup', createdGroups, suite.isMocked);
+        var storageAccountName = suite.generateId('xstorageaccount', createdGroups, suite.isMocked);
+        var storageContainerName = suite.generateId('xstoragecontainer', createdGroups, suite.isMocked);
+        var deploymentName = suite.generateId('Deploy1', createdDeployments, suite.isMocked);
+        //same content like path.join(__dirname, '../../../data/arm-deployment-template.json')
+        //var templateUri = 'http://azuresdkcitest.blob.core.windows.net/azure-cli-test/arm-deployment-template.json';
+        var templateFile = path.join(__dirname, '../../../data/arm-deployment-template.json');
+        var CreateStorageAccount = util.format('storage account create -g %s --sku-name LRS --kind Storage %s --location %s --json', groupName, storageAccountName, 'eastus');
+        var GetKeyString = util.format('storage account keys list %s -g %s --json', storageAccountName, groupName);
+        //var oCreateDeployment = util.format('group deployment create --template-uri %s -g %s -n %s -e %s --nowait --json',
+            //templateUri, groupName, deploymentName, parameterFile);
+        
+        suite.execute('group create %s --location %s --json', groupName, testLocation, function (result) {
+          result.exitStatus.should.equal(0);
+
+          suite.execute(CreateStorageAccount, function (storageResult) {
+            storageResult.exitStatus.should.equal(0);
+
+            suite.execute(GetKeyString, function (keyResult) {
+              var key = JSON.parse(keyResult.text)[0].value;
+              keyResult.exitStatus.should.equal(0);
+
+              suite.execute('storage container create --container %s -a %s -k %s -p Off --json', storageContainerName, storageAccountName, key, function (containerResult) {
+                containerResult.exitStatus.should.equal(0);
+
+                suite.execute('storage blob upload --container %s -f %s -a %s -k %s --json', storageContainerName, templateFile, storageAccountName, key, function (templateResult) { 
+                  templateResult.exitStatus.should.equal(0);
+                  var expireTime = thirtyMinutesFromNow();
+
+                  suite.execute('storage blob sas create --container %s --blob arm-deployment-template.json --permissions r --expiry %s -a %s -k %s --json', storageContainerName, expireTime, storageAccountName, key, function (SASResult) { 
+                    var URIwithSAS = JSON.parse(SASResult.text).url;
+                    SASResult.exitStatus.should.equal(0);                          
+                    // This URIwithSAS was generated on the recorded test if a new test is generated then this variable should be replaced with 
+                    // the new URIwithSAS created in that recorded session. The reason is nock will record requests with the expiration time
+                    // set to thiry minutes after the SAS token generation relative to the time the test was recorded.
+                    URIwithSAS = 'https://xstorageaccount4917.blob.core.windows.net/xstoragecontainer6712/arm-deployment-template.json?se=2016-08-27T02%3A02%3A00Z&sp=r&sv=2015-04-05&sr=b&sig=HjRq7q8o23pXnCyix2ZAJHXPLiuWB5ktibPQn%2FizoKw%3D';
+
+                    suite.execute('group deployment create --template-uri %s -g %s -n %s -e %s --nowait --json', URIwithSAS, groupName, deploymentName, parameterFile, function (deployResult) { 
+                      deployResult.exitStatus.should.equal(0);
+                      var deployment = JSON.parse(deployResult.text);
+                      deployment.name.should.equal(deploymentName);
+                      deployment.id.should.containEql('/resourceGroups/' + groupName);
+     
+                      suite.execute('group deployment stop -g %s -n %s -q --json', groupName, deploymentName, function (listResult) {
+                        listResult.exitStatus.should.equal(0);
+                        cleanup(done);
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+
+      it('should successfully derive name from URI with SAS token', function (done) {
+        var parameterFile = path.join(__dirname, '../../../data/startersite-parameters.json');
+        setUniqParameterNames(suite, parameterFile);
+        var groupName = suite.generateId('xDeploymentTestGroup', createdGroups, suite.isMocked);
+        var storageAccountName = suite.generateId('xstorageaccount', createdGroups, suite.isMocked);
+        var storageContainerName = suite.generateId('xstoragecontainer', createdGroups, suite.isMocked);
+        var deploymentName = 'arm-deployment-template';
+        //same content like path.join(__dirname, '../../../data/arm-deployment-template.json')
+        //var templateUri = 'http://azuresdkcitest.blob.core.windows.net/azure-cli-test/arm-deployment-template.json';
+        var templateFile = path.join(__dirname, '../../../data/arm-deployment-template.json');
+        var CreateStorageAccount = util.format('storage account create -g %s --sku-name LRS --kind Storage %s --location %s --json', groupName, storageAccountName, 'eastus');
+        var GetKeyString = util.format('storage account keys list %s -g %s --json', storageAccountName, groupName);
+        //var oCreateDeployment = util.format('group deployment create --template-uri %s -g %s -n %s -e %s --nowait --json',
+            //templateUri, groupName, deploymentName, parameterFile);
+        
+        suite.execute('group create %s --location %s --json', groupName, testLocation, function (result) {
+          result.exitStatus.should.equal(0);
+
+          suite.execute(CreateStorageAccount, function (storageResult) {
+            storageResult.exitStatus.should.equal(0);
+
+            suite.execute(GetKeyString, function (keyResult) {
+              var key = JSON.parse(keyResult.text)[0].value;
+              keyResult.exitStatus.should.equal(0);
+
+              suite.execute('storage container create --container %s -a %s -k %s -p Off --json', storageContainerName, storageAccountName, key, function (containerResult) {
+                containerResult.exitStatus.should.equal(0);
+
+                suite.execute('storage blob upload --container %s -f %s -a %s -k %s --json', storageContainerName, templateFile, storageAccountName, key, function (templateResult) { 
+                  templateResult.exitStatus.should.equal(0);
+                  var expireTime = thirtyMinutesFromNow();
+
+                  suite.execute('storage blob sas create --container %s --blob arm-deployment-template.json --permissions r --expiry %s -a %s -k %s --json', storageContainerName, expireTime, storageAccountName, key, function (SASResult) { 
+                    var URIwithSAS = JSON.parse(SASResult.text).url;
+                    SASResult.exitStatus.should.equal(0);     
+                    // This URIwithSAS was generated on the recorded test if a new test is generated then this variable should be replaced with 
+                    // the new URIwithSAS created in that recorded session. The reason is nock records the requests with the expiration time 
+                    // set to thiry minutes after the SAS token generation relative to the time the test was recorded.
+                    URIwithSAS = 'https://xstorageaccount714.blob.core.windows.net/xstoragecontainer9648/arm-deployment-template.json?se=2016-08-27T02%3A34%3A00Z&sp=r&sv=2015-04-05&sr=b&sig=H%2B3Fx10IynilQ4hssMCwOlIjmYiBGIUOzGey3pJ5ATI%3D';
+
+                    suite.execute('group deployment create --template-uri %s -g %s -e %s --nowait --json', URIwithSAS, groupName, parameterFile, function (deployResult) { 
+                      deployResult.exitStatus.should.equal(0);
+                      var deployment = JSON.parse(deployResult.text);
+                      deployment.name.should.equal(deploymentName);
+                      deployment.id.should.containEql('/resourceGroups/' + groupName);
+     
+                      suite.execute('group deployment stop -g %s -n %s -q --json', groupName, deploymentName, function (listResult) {
+                        listResult.exitStatus.should.equal(0);
+                        cleanup(done);
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+
 
       it('should all work with a local file', function (done) {
         var parameterFile = path.join(__dirname, '../../../data/arm-deployment-parameters.json');
@@ -337,6 +468,35 @@ describe('arm', function () {
         });
       });
 
+      it('should all work with a local file and v2 parameters that have comments', function (done) {
+        var parameterFile = path.join(__dirname, '../../../data/arm-deployment-parametersv2-with-comments.json');
+        setUniqParameterNames(suite, parameterFile, true);
+        var groupName = suite.generateId('xDeploymentTestGroup', createdGroups, suite.isMocked);
+        var deploymentName = suite.generateId('Deploy1', createdDeployments, suite.isMocked);
+        var templateFile = path.join(__dirname, '../../../data/arm-deployment-template-with-comments.json');
+        var commandToCreateDeployment = util.format('group deployment create -f %s -g %s -n %s -e %s',
+            templateFile, groupName, deploymentName, parameterFile);
+        
+        suite.execute('group create %s --location %s --json', groupName, testLocation, function (result) {
+          result.exitStatus.should.equal(0);
+          suite.execute(commandToCreateDeployment, function (result) {
+            result.exitStatus.should.equal(0);
+            result.text.indexOf('provisioning status is Succeeded').should.be.above(-1);
+
+            suite.execute('group deployment show -g %s -n %s --json', groupName, deploymentName, function (showResult) {
+              showResult.exitStatus.should.equal(0);
+              showResult.text.indexOf(deploymentName).should.be.above(-1);
+              
+              suite.execute('group deployment list -g %s --json', groupName, function (listResult) {
+                listResult.exitStatus.should.equal(0);
+                listResult.text.indexOf(deploymentName).should.be.above(-1);
+                cleanup(done);
+              });
+            });
+          });
+        });
+      });
+
       it('should all work with a string for parameters', function (done) {
         var parameterFile = path.join(__dirname, '../../../data/arm-deployment-parameters.json');
         setUniqParameterNames(suite, parameterFile);
@@ -377,7 +537,7 @@ describe('arm', function () {
           result.exitStatus.should.equal(0);
           suite.execute('group deployment create -f %s -g %s -n %s -p %s --json', templateFile, groupName, deploymentName, parameterString, function (result) {
             result.exitStatus.should.equal(1);
-            result.errorText.should.match(/.*Deployment template validation failed.*/i);
+            result.errorText.should.include("file does not have { siteLocation } defined.");
             cleanup(done);
           });
         });
